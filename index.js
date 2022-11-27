76eff3b9f2eb382d854b20c84a0f87edc1568d81
unknown <ragulpretish@gmail.com>
2022-11-27 20:24:33 +0530

    L9

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./app");
-const port = process.env.PORT || 8742;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Started express server at port 3000- ${port}`);
+  console.log(`server listening at port - ${port}`);
 });
